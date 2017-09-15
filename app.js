f59870458acf699faf788cb367a3ec20157b559c
Johnmani das <johnmani.das@trinet.com>
2017-09-16 01:39:25 +0530

    Moving code to gihub reposity

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var reques = require('request');
+var app = express();
+
+var data;
+app.get('/getCity',function(request,response){
+
+    var url ='https://www.voonik.com/shopping_cart/check_city_state.json?pincode='+request.query.pincode;
+    reques(url,function(error,res,body){
+
+        data = JSON.parse(body);
+        response.send('<h1>State..'+data.state+'..City..'+data.city+'....');
+    })
+});
+
+app.get('/',function(request,response){
+    response.sendFile(__dirname+'/pincode.html');
+})
+
+app.listen(1111);
